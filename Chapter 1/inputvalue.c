015e063ae21407597e0eb7ed028bbb8c0c4de9b6
MdAsifLM <87461866+MdAsifLM@users.noreply.github.com>
2024-07-25 16:38:14 +0530

    Taking Values from user and datatypes

---
+++ Chapter 1/inputvalue.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+    
+    int main () {
+        
+        int a;
+        a=6;
+        printf ("The value of the integer is %d \n", a);
+
+        float b=6.03;
+        printf ("The Value of the Float is %f \n", b);
+
+        char c= 'c';
+        printf ("The Value of the Character is %c \n", c);
+
+    return 0;
+    }
