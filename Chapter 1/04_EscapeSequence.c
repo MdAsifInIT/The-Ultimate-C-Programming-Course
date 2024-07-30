b7eb644ce61891f22d1854bd6a556b323f146085
MdAsifLM <87461866+MdAsifLM@users.noreply.github.com>
2024-07-30 19:22:53 +0530

    new file:   Chapter 1/04_EscapeSequence.c

---
+++ Chapter 1/04_EscapeSequence.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+    // \n is used to print new line
+    printf("He is Drunk. \nI am not Drunk");
+
+    printf("\n");
+
+    // \t is used to give tab space
+    printf("Fruit: \tApple"); 
+
+    printf("\n");
+
+    // \' to give single Quote, \\ for giving slash.
+    printf("Path: \"C:\\Temp\"");
+    
+
+    return 0;
+}
