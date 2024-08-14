9c3f02c4b537434c9a8138ca1bf831a227d6e5a0
MdAsifLM <87461866+MdAsifLM@users.noreply.github.com>
2024-08-14 15:20:53 +0530

    modified:   Chapter 1 Practice Set/02_problem2.c
    Changes not staged for commit:
    	modified:   .gitignore

--- Chapter 1 Practice Set/02_problem2.c
+++ Chapter 1 Practice Set/02_problem2.c
@@ -1,8 +1,15 @@
-//
-
 #include <stdio.h>
 
 int main(){
-    
+    // Area of circle is pi r sq
+    // pi = 3.14
+
+    float radius, pi;
+    pi = 3.14;
+    printf("Enter the Radius of the Circle \n");
+    scanf("%f", &radius);
+
+    printf("Area of the circle is %f sq.units", pi*radius*radius);
+
     return 0;
 }
