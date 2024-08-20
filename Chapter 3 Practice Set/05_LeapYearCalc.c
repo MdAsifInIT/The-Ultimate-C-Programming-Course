35b6ebdd4e809391f707995e391e9cd3bb14d41d
Md Asif <yb.fisa@gmail.com>
2024-08-20 19:31:25 +0530

    new file:   Chapter 3 Practice Set/05_LeapYearCalc.c

---
+++ Chapter 3 Practice Set/05_LeapYearCalc.c
@@ -0,0 +1,11 @@
+/*
+Write a program to find whether a year entered by the user is a leap year or not. 
+Take year as an input from the user.
+*/
+
+#include <stdio.h>
+
+int main(){
+    
+    return 0;
+}
