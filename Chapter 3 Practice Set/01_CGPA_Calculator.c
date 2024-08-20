73c4dd139c8b420b48c89da4a958b08a81ecca83
Md Asif <yb.fisa@gmail.com>
2024-08-20 16:41:07 +0530

    modified:   Chapter 3 Practice Set/01_CGPA_Calculator.c

--- Chapter 3 Practice Set/01_CGPA_Calculator.c
+++ Chapter 3 Practice Set/01_CGPA_Calculator.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
-int main(){
-    float es,wp,ps,sl,total, percentage;
+int main() {
+  float es, wp, ps, sl, total, percentage;
 
-    printf("Enter Complete Electrical Sciences (MarksMidSem+FinalSem+Quiz+Assignment)\n:");
-    scanf("%f", &es);
+  printf("\nEnter Complete Electrical Sciences Marks "
+         "(MidSem+FinalSem+Quiz+Assignment)\n:");
+  scanf("%f", &es);
 
-    printf("Enter Complete Writing Practice (MarksMidSem+FinalSem+Quiz+Assignment)\n:");
-    scanf("%f", &wp);
+  printf("\nEnter Complete Writing Practice Marks "
+         "(MidSem+FinalSem+Quiz+Assignment)\n:");
+  scanf("%f", &wp);
 
-    printf("Enter Complete Probability & Statistics (MarksMidSem+FinalSem+Quiz+Assignment)\n:");
-    scanf("%f", &ps);
+  printf("\nEnter Complete Probability & Statistics Marks "
+         "(MidSem+FinalSem+Quiz+Assignment)\n:");
+  scanf("%f", &ps);
 
-    printf("Enter Complete Symbolic Logic (MarksMidSem+FinalSem+Quiz+Assignment)\n:");
-    scanf("%f", &sl);
+  printf("\nEnter Complete Symbolic Logic Marks "
+         "(MidSem+FinalSem+Quiz+Assignment)\n:");
+  scanf("%f", &sl);
 
-    total = es+wp+ps+sl;
-    percentage = (total/400.0)*100;
+  total = es + wp + ps + sl;
+  percentage = (total / 400.0) * 100;
 
-    printf("\nYou have scored a total of %.2f marks out of 400!\n", total);
-    printf("\nTotal percentage scored is %.2f%%!\n", percentage);
-    printf("\nCongratulations! You have scored %.1f CGPA!\n", percentage/9.5);
-    printf("\nThe Scored are not official by BITS, so don't worry!!\n");
+  printf("\nYou have scored a total of %.2f marks out of 400!\n", total);
+  printf("\nTotal percentage scored is %.2f%%!\n", percentage);
+  printf("\nCongratulations! You have scored %.1f CGPA!\n", percentage / 9.5);
+  printf("\nThe score isn't official by BITS, so don't worry!!\n");
 
-    return 0;
+  if (percentage<=50){
+    printf("But I wonder how someone can score this bad...")
+  }
+
+  return 0;
 }
