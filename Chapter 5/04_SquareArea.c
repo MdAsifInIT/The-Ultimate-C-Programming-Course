055a8de319544ba1a09bdfdfee87d105841e4551
Md Asif <yb.fisa@gmail.com>
2024-08-30 10:40:47 +0530

    modified:   Chapter 5/04_SquareArea.c

--- Chapter 5/04_SquareArea.c
+++ Chapter 5/04_SquareArea.c
@@ -1,12 +1,14 @@
+//Use the library function to calculate the area of a square with side a.
+
 #include <stdio.h>
 #include <math.h>
 
 int main(){
-    double side;
-    printf("Enter the length of side of the square:\n");
-    scanf("%lf", &side);
+    double a;
+    printf("Enter the length of a of the square:\n");
+    scanf("%lf", &a);
 
-    printf("The area of the square is %2.lf square units.\n", pow(side, 2));
+    printf("The area of the square is %2.lf square units.\n", pow(a, 2));
 
     return 0;
 }
