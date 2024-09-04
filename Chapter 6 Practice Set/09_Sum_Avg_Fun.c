1285401cc0de91e0ecb5d552e290e55ee88b3ef5
Md Asif <yb.fisa@gmail.com>
2024-09-04 10:19:01 +0530

    modified:   Chapter 6 Practice Set/09_Sum_Avg_Fun.c

---
+++ Chapter 6 Practice Set/09_Sum_Avg_Fun.c
@@ -0,0 +1,6 @@
+// Print sun and average using the address of a variable in function.
+
+// Original Ques:
+
+// Write a program using a function which calculates the sum and average of two 
+// numbers. Use pointers and print the values of sum and average in main().
