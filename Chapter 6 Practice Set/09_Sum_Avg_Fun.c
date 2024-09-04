d2b33f267141bde8ab91210761cbfa752223192e
Md Asif <yb.fisa@gmail.com>
2024-09-04 20:01:02 +0530

    modified:   Chapter 6 Practice Set/09_Sum_Avg_Fun.c 	new file:   Chapter 7/01_Arrays.c

--- Chapter 6 Practice Set/09_Sum_Avg_Fun.c
+++ Chapter 6 Practice Set/09_Sum_Avg_Fun.c
@@ -4,3 +4,30 @@
 
 // Write a program using a function which calculates the sum and average of two 
 // numbers. Use pointers and print the values of sum and average in main().
+
+#include <stdio.h>
+
+int* sum (int, int);
+int* sum (int a, int b){
+    int c = a + b;
+    int* d = &c;
+    return d;
+}
+
+float* avg (int, int);
+float* avg (int a, int b){
+    float c = (a + b) / 2.0;
+    float* d = &c;
+    return d;
+}
+
+int main(){
+    int a = 5, b = 4;
+    int* s = sum(a,b);
+    printf("Sum = %d \n", *s);
+
+    float* av = avg(a,b);
+    printf("Avg = %f \n", *av);
+
+    return 0;
+}
