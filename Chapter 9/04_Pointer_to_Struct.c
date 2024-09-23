d5fd5b737f9d375f49d5110fd966068e9f135b76
Md Asif <yb.fisa@gmail.com>
2024-09-23 14:07:32 +0530

    modified:   Chapter 9/04_Pointer_to_Struct.c 	new file:   Chapter 9/05_FunctionStructures.c

--- Chapter 9/04_Pointer_to_Struct.c
+++ Chapter 9/04_Pointer_to_Struct.c
@@ -1,6 +1,20 @@
+// Accessing a structure using pointer.
 #include <stdio.h>
 
-int main(){
-    
+struct students
+{
+    int code;
+    float marks;
+};
+
+int main()
+{
+    struct students e1;
+    struct students *ptr;
+    e1.code = 99;
+    ptr = &e1;
+    printf("%d \n", (*ptr).code);
+    // Here we're getting e1 by dereferencing ptr and printing e1.code!
+    printf("%d \n", ptr -> code); //Exactly same as (*ptr).code
     return 0;
 }
---
+++ Chapter 9/05_FunctionStructures.c
@@ -0,0 +1,27 @@
+// Complete this show function to display the content of employee.
+#include <stdio.h>
+#include <string.h>
+
+struct employee {
+    char name[10];
+    int SAP_ID;
+    float Salary_pa;
+};
+
+void show(struct employee e);
+
+void show(struct employee e){
+    printf("Name = %s\n", e.name);
+    printf("SAP_ID = %d\n", e.SAP_ID);
+    printf("Salary_pa = %.2f\n", e.Salary_pa);
+}
+
+int main(){
+    struct employee e1,e2;
+    strcpy(e1.name, "Md Asif");
+    e1.SAP_ID = 69696959;
+    e1.Salary_pa = 6.9;
+
+    show(e1);
+    return 0;
+}
